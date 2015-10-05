f596baab8c1452e3bd9683b47cc8ae90d63f36c0
Christian Rehn <mail@christian-rehn.de>
2015-10-05 17:48:14 +0200

    refactored storeData

--- php/storeData.php
+++ php/storeData.php
@@ -1,158 +1,198 @@
 <?php
+require_once('Logger.php');
+require_once('config.php');
 
-$ukey = $_POST['ukey'];
-$designType = $_POST['designType'];
-$gender = $_POST['gender'];
-$age = $_POST['age'];
-$professionalYears = $_POST['professionalYears'];
-$educationLevel = $_POST['educationLevel'];
-$educationBackground = $_POST['educationBackground'];
-$role = $_POST['role'];
-$programmingLanguage = $_POST['programmingLanguage'];
-$methodology = $_POST['methodology'];
-$companySize = $_POST['companySize'];
-$industrySector = $_POST['industrySector'];
-$latitude = $_POST['latitude'];
-$longitude = $_POST['longitude'];
-
-$boxValueTrue = "true";
-
-//echo "simple1: " . $_POST['simple1'] . "; simple2: " . $_POST['simple2'] . "\n";
-
-$simple1 = (strcmp($_POST['simple1'], $boxValueTrue) == 0) ? 1 : 0;
-$simple2 = (strcmp($_POST['simple2'], $boxValueTrue) == 0) ? 1 : 0;
-$simple3 = (strcmp($_POST['simple3'], $boxValueTrue) == 0) ? 1 : 0;
-$simple4 = (strcmp($_POST['simple4'], $boxValueTrue) == 0) ? 1 : 0;
-$simple5 = (strcmp($_POST['simple5'], $boxValueTrue) == 0) ? 1 : 0;
-$simple6 = (strcmp($_POST['simple6'], $boxValueTrue) == 0) ? 1 : 0;
-$powerful1 = (strcmp($_POST['powerful1'], $boxValueTrue) == 0) ? 1 : 0;
-$powerful2 = (strcmp($_POST['powerful2'], $boxValueTrue) == 0) ? 1 : 0;
-$powerful3 = (strcmp($_POST['powerful3'], $boxValueTrue) == 0) ? 1 : 0;
-$powerful4 = (strcmp($_POST['powerful4'], $boxValueTrue) == 0) ? 1 : 0;
-$powerful5 = (strcmp($_POST['powerful5'], $boxValueTrue) == 0) ? 1 : 0;
-$powerful6 = (strcmp($_POST['powerful6'], $boxValueTrue) == 0) ? 1 : 0;
-$abstract1 = (strcmp($_POST['abstract1'], $boxValueTrue) == 0) ? 1 : 0;
-$abstract2 = (strcmp($_POST['abstract2'], $boxValueTrue) == 0) ? 1 : 0;
-$abstract3 = (strcmp($_POST['abstract3'], $boxValueTrue) == 0) ? 1 : 0;
-$abstract4 = (strcmp($_POST['abstract4'], $boxValueTrue) == 0) ? 1 : 0;
-$abstract5 = (strcmp($_POST['abstract5'], $boxValueTrue) == 0) ? 1 : 0;
-$abstract6 = (strcmp($_POST['abstract6'], $boxValueTrue) == 0) ? 1 : 0;
-$concrete1 = (strcmp($_POST['concrete1'], $boxValueTrue) == 0) ? 1 : 0;
-$concrete2 = (strcmp($_POST['concrete2'], $boxValueTrue) == 0) ? 1 : 0;
-$concrete3 = (strcmp($_POST['concrete3'], $boxValueTrue) == 0) ? 1 : 0;
-$concrete4 = (strcmp($_POST['concrete4'], $boxValueTrue) == 0) ? 1 : 0;
-$concrete5 = (strcmp($_POST['concrete5'], $boxValueTrue) == 0) ? 1 : 0;
-$concrete6 = (strcmp($_POST['concrete6'], $boxValueTrue) == 0) ? 1 : 0;
-$pragmatic1 = (strcmp($_POST['pragmatic1'], $boxValueTrue) == 0) ? 1 : 0;
-$pragmatic2 = (strcmp($_POST['pragmatic2'], $boxValueTrue) == 0) ? 1 : 0;
-$pragmatic3 = (strcmp($_POST['pragmatic3'], $boxValueTrue) == 0) ? 1 : 0;
-$pragmatic4 = (strcmp($_POST['pragmatic4'], $boxValueTrue) == 0) ? 1 : 0;
-$pragmatic5 = (strcmp($_POST['pragmatic5'], $boxValueTrue) == 0) ? 1 : 0;
-$pragmatic6 = (strcmp($_POST['pragmatic6'], $boxValueTrue) == 0) ? 1 : 0;
-$idealistic1 = (strcmp($_POST['idealistic1'], $boxValueTrue) == 0) ? 1 : 0;
-$idealistic2 = (strcmp($_POST['idealistic2'], $boxValueTrue) == 0) ? 1 : 0;
-$idealistic3 = (strcmp($_POST['idealistic3'], $boxValueTrue) == 0) ? 1 : 0;
-$idealistic4 = (strcmp($_POST['idealistic4'], $boxValueTrue) == 0) ? 1 : 0;
-$idealistic5 = (strcmp($_POST['idealistic5'], $boxValueTrue) == 0) ? 1 : 0;
-$idealistic6 = (strcmp($_POST['idealistic6'], $boxValueTrue) == 0) ? 1 : 0;
-$technologic1 = (strcmp($_POST['technologic1'], $boxValueTrue) == 0) ? 1 : 0;
-$technologic2 = (strcmp($_POST['technologic2'], $boxValueTrue) == 0) ? 1 : 0;
-$technologic3 = (strcmp($_POST['technologic3'], $boxValueTrue) == 0) ? 1 : 0;
-$technologic4 = (strcmp($_POST['technologic4'], $boxValueTrue) == 0) ? 1 : 0;
-$technologic5 = (strcmp($_POST['technologic5'], $boxValueTrue) == 0) ? 1 : 0;
-$technologic6 = (strcmp($_POST['technologic6'], $boxValueTrue) == 0) ? 1 : 0;
-$robust1 = (strcmp($_POST['robust1'], $boxValueTrue) == 0) ? 1 : 0;
-$robust2 = (strcmp($_POST['robust2'], $boxValueTrue) == 0) ? 1 : 0;
-$robust3 = (strcmp($_POST['robust3'], $boxValueTrue) == 0) ? 1 : 0;
-$robust4 = (strcmp($_POST['robust4'], $boxValueTrue) == 0) ? 1 : 0;
-$robust5 = (strcmp($_POST['robust5'], $boxValueTrue) == 0) ? 1 : 0;
-$robust6 = (strcmp($_POST['robust6'], $boxValueTrue) == 0) ? 1 : 0;
-
-$userKeyUnique = uniqid();
-
-$httpParams = "ukey: " . $userKeyUnique . "; gender: " . $gender . "; age: " . $age . "; educationLevel: " . $educationLevel . "; role: " . $role . "; latitude: " . $latitude . "; longitude: " . $longitude
-     . "; simple1: " . $simple1 . "; simple2: " . $simple2 . "; simple3: " . $simple3 . "; simple4: " . $simple4 . "; simple5: " . $simple5 . "; simple6: " . $simple6
-     . "; powerful1: " . $powerful1 . "; powerful2: " . $powerful2 . "; powerful3: " . $powerful3 . "; powerful4: " . $powerful4 . "; powerful5: " . $powerful5 . "; powerful6: " . $powerful6
-     . "; abstract1: " . $abstract1 . "; abstract2: " . $abstract2 . "; abstract3: " . $abstract3 . "; abstract4: " . $abstract4 . "; abstract5: " . $abstract5 . "; abstract6: " . $abstract6
-     . "; concrete1: " . $concrete1 . "; concrete2: " . $concrete2 . "; concrete3: " . $concrete3 . "; concrete4: " . $concrete4 . "; concrete5: " . $concrete5 . "; concrete6: " . $concrete6
-     . "; pragmatic1: " . $pragmatic1 . "; pragmatic2: " . $pragmatic2 . "; pragmatic3: " . $pragmatic3 . "; pragmatic4: " . $pragmatic4 . "; pragmatic5: " . $pragmatic5 . "; pragmatic6: " . $pragmatic6
-     . "; idealistic1: " . $idealistic1 . "; idealistic2: " . $idealistic2 . "; idealistic3: " . $idealistic3 . "; idealistic4: " . $idealistic4 . "; idealistic5: " . $idealistic5 . "; idealistic6: " . $idealistic6
-     . "; technologic1: " . $technologic1 . "; technologic2: " . $technologic2 . "; technologic3: " . $technologic3 . "; technologic4: " . $technologic4 . "; technologic5: " . $technologic5 . "; technologic6: " . $technologic6
-     . "; robust1: " . $robust1 . "; robust2: " . $robust2 . "; robust3: " . $robust3 . "; robust4: " . $robust4 . "; robust5: " . $robust5 . "; robust6: " . $robust6 . "\n";
-
-//echo $httpParams;
-$file = 'mw_php.log';
-file_put_contents($file, $httpParams, FILE_APPEND);
-
-// connect to db
-$config_ini = parse_ini_file("./dbconfig.ini");
-$mysqli = new mysqli($config_ini['host'], $config_ini['user'], $config_ini['pwd'], $config_ini['dbname']);
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to database: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+$log = new Logger(basename(__FILE__, ".php"));
+if (DEBUG) {
+    $log->setLogLevel(LogLevel::debug());
 }
 
-$tablenameCS = $config_ini['tableprefix'] . "ChoosenStatements";
-$tablenameRT = $config_ini['tableprefix'] . "ResultType";
-
-//insert result type and statistic data
-if (!($stmt = $mysqli->prepare("INSERT INTO " . $tablenameRT . "(USERKEY, DESIGN_TYPE, GENDER, AGE, PROF_YEARS, EDU_LEVEL, 
-                                                       EDU_BACKGROUND, ROLE, PROG_LANG, METHODOLOGY, LATITUDE, LONGITUDE, 
-                                                       COMPANY_SIZE, INDUSTRY_SECTOR) 
-                                                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"))) {
-    echo "Prepare for insert failed: (" . $mysqli->errno . ") " . $mysqli->error;
+function storeData() {
+    global $log;
+    try {
+        $ukey = uniqid();
+        $log->info("store data for ukey [$ukey]");
+        $mysqli = connectToDb();
+        $resultTypeFk = insertResultType($mysqli, $ukey);
+        insertChosenStatements($mysqli, $resultTypeFk);
+        echo json_encode($ukey);
+    } finally {
+        $mysqli->close();
+    }
 }
 
-if (!$stmt->bind_param("ssssssssssddss", $userKeyUnique, $designType, $gender, $age, $professionalYears, 
-                                         $educationLevel, $educationBackground, $role, $programmingLanguage, 
-                                         $methodology, $latitude, $longitude, $companySize, $industrySector)) {
-    echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+function connectToDb() {
+    global $log;
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if ($mysqli->connect_errno) {
+        $log->error("Failed to connect to database: ({$mysqli->connect_errno}) {$mysqli->connect_error}");
+        error500();
+    }
+    return $mysqli;
 }
 
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+function error500() {
+    global $log;
+    header("HTTP/1.0 500 Internal Server Error");
+    die("Cannot store data; requestId: " . $log->getRequestId());
 }
 
-$fkType = $stmt->insert_id;
-//echo "FK: " . $fkType . "\n";
+function insertResultType($mysqli, $ukey) {
+    global $log;
+    
+    try {
+        $tablenameRT = DB_TABLEPREFIX . "ResultType";
+        $insertStatement = "INSERT INTO $tablenameRT (USERKEY, DESIGN_TYPE, GENDER, AGE, PROF_YEARS, EDU_LEVEL, 
+            EDU_BACKGROUND, ROLE, PROG_LANG, METHODOLOGY, LATITUDE, LONGITUDE, 
+            COMPANY_SIZE, INDUSTRY_SECTOR) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $log->debug($insertStatement);
+        if (!($stmt = $mysqli->prepare($insertStatement))) {
+            $log->error("Prepare for insert failed: ({$mysqli->errno}) {$mysqli->error}");
+            error500();
+        }
 
-// insert specific choosen statements
-if (!($stmt2 = $mysqli->prepare("INSERT INTO " . $tablenameCS . "(SIMPLE_1, SIMPLE_2, SIMPLE_3, SIMPLE_4, SIMPLE_5, SIMPLE_6,
-                                                        POWERFUL_1, POWERFUL_2, POWERFUL_3, POWERFUL_4, POWERFUL_5, POWERFUL_6,
-                                                        ABSTRACT_1, ABSTRACT_2, ABSTRACT_3, ABSTRACT_4, ABSTRACT_5, ABSTRACT_6,
-                                                        CONCRETE_1, CONCRETE_2, CONCRETE_3, CONCRETE_4, CONCRETE_5, CONCRETE_6,
-                                                        PRAGMATIC_1, PRAGMATIC_2, PRAGMATIC_3, PRAGMATIC_4, PRAGMATIC_5, PRAGMATIC_6,
-                                                        IDEALISTIC_1, IDEALISTIC_2, IDEALISTIC_3, IDEALISTIC_4, IDEALISTIC_5, IDEALISTIC_6,
-                                                        TECHNOLOGIC_1, TECHNOLOGIC_2, TECHNOLOGIC_3, TECHNOLOGIC_4, TECHNOLOGIC_5, TECHNOLOGIC_6,
-                                                        ROBUST_1, ROBUST_2, ROBUST_3, ROBUST_4, ROBUST_5, ROBUST_6,
-                                                        FK) 
-                                                       VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"))) {
-    echo "Prepare for insert failed: (" . $mysqli->errno . ") " . $mysqli->error;
+        $designType = getData('designType');
+        $gender = getData('gender');
+        $age = getData('age');
+        $professionalYears = getData('professionalYears');
+        $educationLevel = getData('educationLevel');
+        $educationBackground = getData('educationBackground');
+        $role = getData('role');
+        $programmingLanguage = getData('programmingLanguage');
+        $methodology = getData('methodology');
+        $companySize = getData('companySize');
+        $industrySector = getData('industrySector');
+        $latitude = getData('latitude');
+        $longitude = getData('longitude');
+
+        $log->info("ukey=$ukey, designType=$designType, gender=$gender, age=$age, professionalYears=$professionalYears,"
+             . "educationLevel=$educationLevel, educationBackground=$educationBackground, role=$role, "
+             . "programmingLanguage=$programmingLanguage, methodology=$methodology, companySize=$companySize, "
+             . "industrySector=$industrySector, latitude=$latitude, longitude=$longitude");
+        if (!$stmt->bind_param("ssssssssssddss", 
+            $ukey, $designType, $gender, $age, $professionalYears, $educationLevel, $educationBackground, $role, 
+            $programmingLanguage, $methodology, $latitude, $longitude, $companySize, $industrySector)) {
+            $log->error("Binding parameters failed: ({$mysqli->errno}) {$mysqli->error}");
+            error500();
+        }
+
+        if (!$stmt->execute()) {
+            $log->error("Execute failed: ({$mysqli->errno}) {$mysqli->error}");
+            error500();
+        }
+
+        $insertId = $stmt->insert_id;
+        $log->debug("inserted record with id [$insertId] into table [$tablenameRT]");
+        return $insertId; //TODO: use ukey to correlate both tables
+    } finally {
+        $stmt->close();
+    }
 }
 
-if (!$stmt2->bind_param("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", $simple1, $simple2, $simple3, $simple4, $simple5, $simple6,
-                                         $powerful1, $powerful2, $powerful3, $powerful4, $powerful5, $powerful6, 
-                                         $abstract1, $abstract2, $abstract3, $abstract4, $abstract5, $abstract6, 
-                                         $concrete1, $concrete2, $concrete3, $concrete4, $concrete5, $concrete6, 
-                                         $pragmatic1, $pragmatic2, $pragmatic3, $pragmatic4, $pragmatic5, $pragmatic6, 
-                                         $idealistic1, $idealistic2, $idealistic3, $idealistic4, $idealistic5, $idealistic6, 
-                                         $technologic1, $technologic2, $technologic3, $technologic4, $technologic5, $technologic6, 
-                                         $robust1, $robust2, $robust3, $robust4, $robust5, $robust6, 
-                                         $fkType)) {
-    echo "Binding parameters failed: (" . $stmt2->errno . ") " . $stmt2->error;
+function getData($key) {
+    //TODO: add proper sanitization
+    return $_POST[$key];
 }
 
-if (!$stmt2->execute()) {
-    echo "Execute failed: (" . $stmt2->errno . ") " . $stmt2->error;
+function insertChosenStatements($mysqli, $resultTypeFk) {
+    global $log;
+    try {
+        $tablenameCS = DB_TABLEPREFIX . "ChoosenStatements"; //TODO: correct typo in table name
+
+        //TODO: consider a separate table for the statements; that way there could be additional data for the statements; furthermore there wouldn't be that many cols
+        $insertStatement = "INSERT INTO $tablenameCS (SIMPLE_1, SIMPLE_2, SIMPLE_3, SIMPLE_4, SIMPLE_5, SIMPLE_6,
+            POWERFUL_1, POWERFUL_2, POWERFUL_3, POWERFUL_4, POWERFUL_5, POWERFUL_6,
+            ABSTRACT_1, ABSTRACT_2, ABSTRACT_3, ABSTRACT_4, ABSTRACT_5, ABSTRACT_6,
+            CONCRETE_1, CONCRETE_2, CONCRETE_3, CONCRETE_4, CONCRETE_5, CONCRETE_6,
+            PRAGMATIC_1, PRAGMATIC_2, PRAGMATIC_3, PRAGMATIC_4, PRAGMATIC_5, PRAGMATIC_6,
+            IDEALISTIC_1, IDEALISTIC_2, IDEALISTIC_3, IDEALISTIC_4, IDEALISTIC_5, IDEALISTIC_6,
+            TECHNOLOGIC_1, TECHNOLOGIC_2, TECHNOLOGIC_3, TECHNOLOGIC_4, TECHNOLOGIC_5, TECHNOLOGIC_6,
+            ROBUST_1, ROBUST_2, ROBUST_3, ROBUST_4, ROBUST_5, ROBUST_6,
+            FK) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $log->debug($insertStatement);
+        if (!($stmt = $mysqli->prepare($insertStatement))) {
+            $log->error("Prepare for insert failed: ({$mysqli->errno}) {$mysqli->error}");
+            error500();
+        }
+
+        if (!$stmt->bind_param("iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii", 
+            statementAsInt('simple1'),
+            statementAsInt('simple2'),
+            statementAsInt('simple3'),
+            statementAsInt('simple4'),
+            statementAsInt('simple5'),
+            statementAsInt('simple6'),
+            
+            statementAsInt('powerful1'),
+            statementAsInt('powerful2'),
+            statementAsInt('powerful3'),
+            statementAsInt('powerful4'),
+            statementAsInt('powerful5'),
+            statementAsInt('powerful6'),
+            
+            statementAsInt('abstract1'),
+            statementAsInt('abstract2'),
+            statementAsInt('abstract3'),
+            statementAsInt('abstract4'),
+            statementAsInt('abstract5'),
+            statementAsInt('abstract6'),
+            
+            statementAsInt('concrete1'),
+            statementAsInt('concrete2'),
+            statementAsInt('concrete3'),
+            statementAsInt('concrete4'),
+            statementAsInt('concrete5'),
+            statementAsInt('concrete6'),
+            
+            statementAsInt('pragmatic1'),
+            statementAsInt('pragmatic2'),
+            statementAsInt('pragmatic3'),
+            statementAsInt('pragmatic4'),
+            statementAsInt('pragmatic5'),
+            statementAsInt('pragmatic6'),
+            
+            statementAsInt('idealistic1'),
+            statementAsInt('idealistic2'),
+            statementAsInt('idealistic3'),
+            statementAsInt('idealistic4'),
+            statementAsInt('idealistic5'),
+            statementAsInt('idealistic6'),
+            
+            statementAsInt('technologic1'),
+            statementAsInt('technologic2'),
+            statementAsInt('technologic3'),
+            statementAsInt('technologic4'),
+            statementAsInt('technologic5'),
+            statementAsInt('technologic6'),
+            
+            statementAsInt('robust1'),
+            statementAsInt('robust2'),
+            statementAsInt('robust3'),
+            statementAsInt('robust4'),
+            statementAsInt('robust5'),
+            statementAsInt('robust6'),
+            $resultTypeFk)) {
+            $log->error("Binding parameters failed: ({$mysqli->errno}) {$mysqli->error}");
+            error500();
+        }
+
+        if (!$stmt->execute()) {
+            $log->error("Execute failed: ({$mysqli->errno}) {$mysqli->error}");
+            error500();
+        }
+        
+        $insertId = $stmt->insert_id;
+        $log->debug("inserted record with id [$insertId] into table [$tablenameCS]");
+    } finally {
+        $stmt->close();
+    }
 }
 
+function statementAsInt($statementKey) {
+    return ($_POST[$statementKey] === "true") ? 1 : 0;
+}
 
-// close statement and connection
-$stmt->close();
-$stmt2->close();
-$mysqli->close();
-
-
-// deliver user key for recognization
-echo json_encode($userKeyUnique);
-
+storeData();
 ?>
