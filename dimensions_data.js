ed5bd0de9c0e2ababc2f2aef8d6e68eeadc1ada2
Matthias Wittum <witty.access@gmx.net>
2015-10-06 23:25:46 +0200

    added new legend icons and pimped up result page, types page and certificate page

---
+++ dimensions_data.js
@@ -0,0 +1,43 @@
+var dimensionsData = {
+simple: {
+    name: 'simple',
+    shortdescription: "This means you prefer simplicity",
+    icondescription: 'A lance is equipped with a simple blade on top of a wooden stick - simply working.',
+},
+powerful: {
+    name: 'powerful',
+    shortdescription: "This means you favor flexible, generalized solutions",
+    icondescription: 'An halberd comes with additional blades that make it more powerful than e.g. a lance.',
+},
+abstract: {
+    name: 'abstract',
+    shortdescription: "This means you like to get an overall overview",
+    icondescription: 'An eagle flys high in the air where it has a perfect overview over everything.',
+},
+concrete: {
+    name: 'concrete',
+    shortdescription: "This means you care about the very details of your software",
+    icondescription: 'A fish is perfectly adapted to its environment the water. One would say in its element.',
+},
+pragmatic: {
+    name: 'pragmatic',
+    shortdescription: "This means you like getting things done fast",
+    icondescription: 'The money bag stands for focussing on customer needs which will fill it.',
+},
+idealistic: {
+    name: 'idealistic',
+    shortdescription: "This means you try to achieve perfection",
+    icondescription: 'The cross stands for believing ones that can move mountains.',
+},
+robust: {
+    name: 'robust',
+    shortdescription: "This means you strive for stable and robust software",
+    icondescription: 'The tower is a building that stands for stability nobody can tear it down.',
+},
+technologic: {
+    name: 'technologic',
+    shortdescription: "This means you are open minded for improvement",
+    icondescription: 'The gun stands for technological improvement that enabled to tear walls down easily.',
+}
+};
+
