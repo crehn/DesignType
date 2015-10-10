e7f7fd9b44bfea92b5d4cc82fbcbfd50b45e83e9
Christian Rehn <mail@christian-rehn.de>
2015-10-10 23:31:48 +0200

    updated dimension data

--- dimensions_data.js
+++ dimensions_data.js
@@ -1,43 +1,43 @@
 var dimensionsData = {
-simple: {
-    name: 'simple',
-    shortdescription: "This means you prefer simplicity",
-    icondescription: 'A lance is equipped with a simple blade on top of a wooden stick - simply working.',
-},
-powerful: {
-    name: 'powerful',
-    shortdescription: "This means you favor flexible, generalized solutions",
-    icondescription: 'An halberd comes with additional blades that make it more powerful than e.g. a lance.',
-},
-abstract: {
-    name: 'abstract',
-    shortdescription: "This means you like to get an overall overview",
-    icondescription: 'An eagle flys high in the air where it has a perfect overview over everything.',
-},
-concrete: {
-    name: 'concrete',
-    shortdescription: "This means you care about the very details of your software",
-    icondescription: 'A fish is perfectly adapted to its environment the water. One would say in its element.',
-},
-pragmatic: {
-    name: 'pragmatic',
-    shortdescription: "This means you like getting things done fast",
-    icondescription: 'The money bag stands for focussing on customer needs which will fill it.',
-},
-idealistic: {
-    name: 'idealistic',
-    shortdescription: "This means you try to achieve perfection",
-    icondescription: 'The cross stands for believing ones that can move mountains.',
-},
-robust: {
-    name: 'robust',
-    shortdescription: "This means you strive for stable and robust software",
-    icondescription: 'The tower is a building that stands for stability nobody can tear it down.',
-},
-technologic: {
-    name: 'technologic',
-    shortdescription: "This means you are open minded for improvement",
-    icondescription: 'The gun stands for technological improvement that enabled to tear walls down easily.',
-}
+    simple: {
+        name: 'simple',
+        shortdescription: "This means you prefer simple, streight-forward solutions",
+        icondescription: 'A pike is very simple, yet effective weapon',
+    },
+    powerful: {
+        name: 'powerful',
+        shortdescription: "This means you favor flexible, generalized solutions",
+        icondescription: 'An halberd consists of an exe blade, a spike, and a hook or thorn and can be used in various ways',
+    },
+    abstract: {
+        name: 'abstract',
+        shortdescription: "This means you always have the big picture in mind",
+        icondescription: 'An eagle flys high in the air where it has a perfect overview',
+    },
+    concrete: {
+        name: 'concrete',
+        shortdescription: "This means you care about the very details of your software",
+        icondescription: 'Code is to a concrete developer like water to a fish',
+    },
+    pragmatic: {
+        name: 'pragmatic',
+        shortdescription: "This means you like getting things done fast",
+        icondescription: 'The money bag stands for focussing on customer needs',
+    },
+    idealistic: {
+        name: 'idealistic',
+        shortdescription: "This means you try to achieve perfection",
+        icondescription: 'The cross symbolizes that you believe in developing the right way',
+    },
+    robust: {
+        name: 'robust',
+        shortdescription: "This means you strive for stable and robust software",
+        icondescription: 'The tower is a building that stands for stability',
+    },
+    technologic: {
+        name: 'technologic',
+        shortdescription: "This means you are open minded for improvement",
+        icondescription: 'The canon stands for technological improvement',
+    }
 };
 
