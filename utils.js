ccfde3b5228bebb012697b7b90f747e88cecf116
Christian Rehn <mail@christian-rehn.de>
2018-03-18 12:45:18 +0100

    matrix: basic concepts and picture

--- utils.js
+++ utils.js
@@ -6,15 +6,22 @@ function getHttpParameter(key) {
     var callUrl = window.location.search.substring(1);
     var urlVars = callUrl.split('&');
     for (var i = 0; i < urlVars.length; i++) {
-       var nameValue = urlVars[i].split('=');
-       if (nameValue[0] == key) {
-          return nameValue[1];
-       }
-    }
- }
- 
-function debuglog(msg) {
-    if (debug) { 
-        console.log(msg); 
+        var nameValue = urlVars[i].split('=');
+        if (nameValue[0] == key) {
+            return nameValue[1];
+        }
     }
 }
+
+function debuglog(msg) {
+    if (debug) {
+        console.log(msg);
+    }
+}
+
+$(function () {
+    $('.pop').on('click', function () {
+        $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+        $('#imagemodal').modal('show');
+    });
+});
