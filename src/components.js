76f7732826bb8ce8245fdf6259e3742742af50e9
Christian Rehn <mail@christian-rehn.de>
2018-12-13 21:56:40 +0100

    removed xing link (and obsolete images)

--- src/components.js
+++ src/components.js
@@ -368,9 +368,6 @@ class DtFooter extends CustomHtmlElement {
             <div>
                 <a class="feedback">E-Mail</a> |
                 <a href="https://twitter.com/SWDesignKnights">Twitter</a> |
-                <a href="https://www.xing.com/communities/groups/design-principles-patterns-types-08d2-1080100">
-                    Xing
-                </a> |
                 <a href="privacy.html">Privacy</a> |
                 <a href="imprint.html">Imprint</a>
             </div>
