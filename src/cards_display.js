940f5c1f7d180afb85b295636d3c615f482b833d
Christian Rehn <mail@christian-rehn.de>
2019-05-01 21:51:14 +0200

    formatting and a little refactoring

--- src/cards_display.js
+++ src/cards_display.js
@@ -1,7 +1,5 @@
-
 function fillPageWithContent(cardname) {
-	
-	var designCardObj = getCardObject(cardname);
+    var designCardObj = getCardObject(cardname);
 
     putImageSourcesIntoPage(designCardObj);
 
@@ -11,16 +9,16 @@ function fillPageWithContent(cardname) {
         putReferencedArgumentsIntoPage(designCardObj.links);
         loadContentFromPrinciplesWikiIntoPage(designCardObj.principlesWikiUrl);
         putDimensionInfoIntoPage(designCardObj, true);
-	} else {
-	    debuglog("hide card argument parts for non argument cards.");
+    } else {
+        debuglog("hide card argument parts for non argument cards.");
         hideNonArgumentParts();
         putDimensionInfoIntoPage(designCardObj, false);
-	}
+    }
 
-	putCommentIdIntoPage(designCardObj);
+    putCommentIdIntoPage(designCardObj);
 }
 
-function putDimensionInfoIntoPage(designCardObj, isArgumentCard){
+function putDimensionInfoIntoPage(designCardObj, isArgumentCard) {
     if (isArgumentCard) {
         var dimensionLink = "./dimensions.html?dimension=" + designCardObj.aspect;
         $('#dimension').append("<a id=\"dimensionlink\" href=\"" + dimensionLink + "\">" + designCardObj.aspect + "</a>");
@@ -29,7 +27,7 @@ function putDimensionInfoIntoPage(designCardObj, isArgumentCard){
     }
 }
 
-function getCardObject(cardname){
+function getCardObject(cardname) {
     var designCardObj = cardsData[cardname];
     if (typeof designCardObj != "undefined") {
         debuglog("Found design card object with name: " + designCardObj.name);
@@ -40,7 +38,7 @@ function getCardObject(cardname){
     }
 }
 
-function putCommentIdIntoPage(designCardObj){
+function putCommentIdIntoPage(designCardObj) {
     debuglog("dynamically change comment pageid to: " + designCardObj.commentId);
     $('dt-comments').attr("pageid", designCardObj.commentId);
 }
@@ -51,28 +49,21 @@ function hideNonArgumentParts() {
 }
 
 function isArgument(cardAspect) {
-    if (typeof cardAspect != "undefined"
-        && (cardAspect == "simple" || cardAspect == "powerful"
-            || cardAspect == "abstract" || cardAspect == "concrete"
-            || cardAspect == "pragmatic" || cardAspect == "idealistic"
-            || cardAspect == "robust" || cardAspect == "technologic")) {
-        return true;
-    } else {
-        return false;
-    }
+    const argumentAspects = ['simple', 'powerful', 'abstract', 'concrete', 'pragmatic', 'idealistic', 'robust', 'technologic'];
+    return argumentAspects.includes(cardAspect);
 }
 
 function loadContentFromPrinciplesWikiIntoPage(principlesWikiUrl) {
     if (principlesWikiUrl != 'none') {
         var corsProblemSolverUrl = 'http://www.whateverorigin.org/get?url=';
         var correspondingWebsite = encodeURIComponent(principlesWikiUrl);
-        $.getJSON(corsProblemSolverUrl + correspondingWebsite + '&callback=?', function(data){
+        $.getJSON(corsProblemSolverUrl + correspondingWebsite + '&callback=?', function (data) {
             //debuglog("loaded: " + data.contents);
-            var rationaleDiv = $($.parseHTML(data.contents)).find("h2:contains('Rationale')").next('.level2')
+            var rationaleDiv = $($.parseHTML(data.contents)).find("h2:contains('Rationale')").next('.level2');
             //debuglog(rationaleDiv);
             $('.rationale').append('<h2>Rationale</h2>');
             $('.rationale').append(rationaleDiv);
-            var strategiesDiv = $($.parseHTML(data.contents)).find("h2:contains('Strategies')").next('.level2')
+            var strategiesDiv = $($.parseHTML(data.contents)).find("h2:contains('Strategies')").next('.level2');
             //debuglog(strategiesDiv);
             $('.strategies').append('<h2>Strategies</h2>');
             $('.strategies').append(strategiesDiv);
@@ -85,39 +76,39 @@ function loadContentFromPrinciplesWikiIntoPage(principlesWikiUrl) {
 }
 
 function putReferencedArgumentsIntoPage(refLinks) {
-	if (typeof refLinks != "undefined") {
-		var tableLinks = "<table class='table table-striped table-light'><thead class='header-table-cardrefs'><tr>";
-		//tableLinks += "<td>Type</td><td>Abbreviation</td><td>Name</td><td>Set</td><td>Notes</td>";
-		tableLinks += "<td>Type</td><td>Abbreviation</td><td>Name</td>";
-		tableLinks += "</tr></thead>";
-		tableLinks += "<tbody>";
-		for (i = 0; i < refLinks.length; i++) {
-			var link = refLinks[i];
-			//debuglog(link.type + "|" + link.abbreviation + "|" + link.name + "|" + link.set + "|" + link.notes);
-			//tableLinks += "<tr><td>" + link.type + "</td>" + "<td><a href='./" + link.abbreviation + ".html'>" + link.abbreviation + "</td>" + "<td>" + link.name + "</a></td>" + "<td>" + link.set + "</td>" + "<td>" + link.notes + "</td></tr>";
-			tableLinks += "<tr><td>" + link.type + "</td>" + "<td><a href='./" + link.abbreviation + ".html'>" + link.abbreviation + "</td>" + "<td>" + link.name + "</a></td></tr>";
-		}
-		tableLinks += "</tbody></table>";
-		$('.references').append(tableLinks);
-	} else {
+    if (typeof refLinks != "undefined") {
+        var tableLinks = "<table class='table table-striped table-light'><thead class='header-table-cardrefs'><tr>";
+        //tableLinks += "<td>Type</td><td>Abbreviation</td><td>Name</td><td>Set</td><td>Notes</td>";
+        tableLinks += "<td>Type</td><td>Abbreviation</td><td>Name</td>";
+        tableLinks += "</tr></thead>";
+        tableLinks += "<tbody>";
+        for (i = 0; i < refLinks.length; i++) {
+            var link = refLinks[i];
+            //debuglog(link.type + "|" + link.abbreviation + "|" + link.name + "|" + link.set + "|" + link.notes);
+            //tableLinks += "<tr><td>" + link.type + "</td>" + "<td><a href='./" + link.abbreviation + ".html'>" + link.abbreviation + "</td>" + "<td>" + link.name + "</a></td>" + "<td>" + link.set + "</td>" + "<td>" + link.notes + "</td></tr>";
+            tableLinks += "<tr><td>" + link.type + "</td>" + "<td><a href='./" + link.abbreviation + ".html'>" + link.abbreviation + "</td>" + "<td>" + link.name + "</a></td></tr>";
+        }
+        tableLinks += "</tbody></table>";
+        $('.references').append(tableLinks);
+    } else {
         $('#referencedargs').hide();
     }
 }
 
 function putCardsContentIntoPage(designCardObj) {
-	$('title').text(designCardObj.abbreviation);
-	$('.c_cardname').text(designCardObj.name);
-	$('#acronym').text(designCardObj.abbreviation);
-	$('#cardname').text(designCardObj.name);
-	$('#level').text(designCardObj.set);
-	$('#shortdesc').text(designCardObj.catchphrase);
-	$('#longdesc').text(designCardObj.description);
+    $('title').text(designCardObj.abbreviation);
+    $('.c_cardname').text(designCardObj.name);
+    $('#acronym').text(designCardObj.abbreviation);
+    $('#cardname').text(designCardObj.name);
+    $('#level').text(designCardObj.set);
+    $('#shortdesc').text(designCardObj.catchphrase);
+    $('#longdesc').text(designCardObj.description);
 }
 
 function putImageSourcesIntoPage(designCardObj) {
-	$('#cardpicture').attr("src", designCardObj.pathToPicture);
-	$('#levelpicture').attr("src", designCardObj.setIcon);
-	$('#dimensionpicture').attr("src", designCardObj.aspectIcon);
+    $('#cardpicture').attr("src", designCardObj.pathToPicture);
+    $('#levelpicture').attr("src", designCardObj.setIcon);
+    $('#dimensionpicture').attr("src", designCardObj.aspectIcon);
 }
 
 function investigateCardnameFromUrl() {
@@ -125,19 +116,18 @@ function investigateCardnameFromUrl() {
     var cardname;
     // just one condition: html file/URL ending name MUST match string index of array that contains all design card arguments
     if (url.lastIndexOf('?cardid=') != -1) {
-        cardname = url.substring(url.lastIndexOf('=')+1, url.length);
+        cardname = url.substring(url.lastIndexOf('=') + 1, url.length);
     } else if (url.lastIndexOf('.html') != -1) {
-        cardname = url.substring(url.lastIndexOf('/')+1, url.lastIndexOf('.'));
+        cardname = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.'));
     } else {
-        cardname = url.substring(url.lastIndexOf('/')+1, url.length);
+        cardname = url.substring(url.lastIndexOf('/') + 1, url.length);
     }
     debuglog("found cardname: " + cardname + " using url: " + url);
     return cardname;
 }
 
 $(document).ready(function () {
+    var cardname = investigateCardnameFromUrl();
 
-	var cardname = investigateCardnameFromUrl();
-
-	fillPageWithContent(cardname);
+    fillPageWithContent(cardname);
 });
